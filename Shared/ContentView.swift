8e64e8eebf37c78a9706008c8f3b0f39d6890e24
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-08-11 12:06:08 -0700

    :1234: Add a display to show the counter's value

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Create a state variable to store our counter's numeric value.
+    @State var counter_display: Int = 0
+    
+    // This is the main view.
     var body: some View {
+        
+        // Use a vertically stacked layout.
         VStack(alignment: .center) {
-        Text("Hello, world!")
-            .padding()
+            
+            // This is the display for the counter's numeric value.
+            Text("\(counter_display)")
+                .padding()
         }
     }
 }

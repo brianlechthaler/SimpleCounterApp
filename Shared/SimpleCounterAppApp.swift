c1d7b38dfcaf8d2d41e141a0dddfd5612fe5caa2
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-08-11 11:52:34 -0700

    Initial Commit

---
+++ Shared/SimpleCounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterAppApp.swift
+//  Shared
+//
+//  Created by Brian Lechthaler on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
